9ec37ae853c0fbbf39a1c2fb1c4e1e86edb904c7
Abhilash Srivastava <abhilashsrivastava17@gmail.com>
2018-08-21 15:39:45 +0530

    Add Get-Route Function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-export const sum = (a, b) => {
-  return (a + b)
+"use strict";
+
+module.exports = function getRoute(url) {
+  const match = url.match(
+    /^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/
+  )
+  return (match && {
+    href: href,
+    protocol: match[1],
+    host: match[2],
+    hostname: match[3],
+    port: match[4],
+    pathname: match[5],
+    search: match[6],
+    hash: match[7]
+  }) 
 }
